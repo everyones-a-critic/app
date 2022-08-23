d7df5a90e6da533e1c717d9cf593dcf06ade2ca2
Daniel Long <24.daniel.long@gmail.com>
2022-08-23 11:37:11 -0600

    Completes Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,114 @@
+import React, { createRef } from "react";
+import { View, Animated, StyleSheet } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faSpinner } from '@fortawesome/free-solid-svg-icons/faSpinner'
+
+
+class Loader extends React.Component {
+    state = {
+        spinValue: new Animated.Value(0),
+    };
+
+    ref = React.createRef();
+
+    rotateSpinner = () => {
+        Animated.loop(
+            Animated.timing(this.state.spinValue, {
+                toValue: 1,
+                duration: 1200,
+                useNativeDriver: false  // To make use of native driver for performance
+            })
+        ).start();
+    }
+
+    spin = this.state.spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
+
+    initiateSpinner = () => {
+        if (this.props.loading) {
+            this.timer = setTimeout(() => {
+                this.ref.current.setNativeProps({ style: { display: 'flex' }});
+                this.rotateSpinner();
+            }, 750);
+        }
+    }
+
+    componentDidMount = () => {
+        this.initiateSpinner();
+    }
+
+    componentDidUpdate = () => {
+        this.initiateSpinner();
+    }
+
+    renderSpinner = () => {
+        if (this.props.loading) {
+            return (
+                <React.Fragment>
+                    <View style={[ style.absolute, style.overlay ]} accessibilityLabel="Content Cover" />
+                    <View style={[ style.absolute, style.spinnerContainer ]} accessibilityLabel="Loading">
+                        <Animated.Text testID={"spinner"} ref={this.ref} style={[
+                            style.spinner,
+                            { display: 'none'},
+                            {transform: [{ rotate: this.spin }]}
+                        ]}>
+                            <FontAwesomeIcon size={ 50 } color={'black'}  icon={faSpinner} />
+                        </Animated.Text>
+                    </View>
+                </React.Fragment>
+            )
+        }
+    }
+
+    render = () => {
+        return (
+            <View style={{ flex: 1}}>
+                { this.renderSpinner() }
+                <View style={[ style.absolute, style.container ]}>
+                    { this.props.children }
+                </View>
+            </View>
+        );
+    }
+}
+
+const style = StyleSheet.create({
+    absolute: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+    },
+    spinnerContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 2
+    },
+    spinner: {
+        width: 50,
+        height: 45,
+    },
+    overlay: {
+        zIndex: 1,
+        backgroundColor: 'black',
+        opacity: 0.4,
+    },
+    container: {
+        zIndex: 0,
+    }
+});
+
+export default Loader;
+
+// on render, if props.loading == false
+//    do nothing
+// else
+//    render background
+//    if spinnerVisible = true
+//        render Spinner
+//        start spinning
+//    else
+//        750m timeout set spinnerVisible = true
---
+++ src/components/Loader.test.js
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react-native';
+import {Text} from "react-native";
+
+import Loader from "./Loader";
+
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+    FontAwesomeIcon: ''
+}))
+
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
+
+describe('when loading is', () => {
+    test('false, spinner should not render, but content should still render', ()=> {
+        render(<Loader loading={false}><Text accessibilityRole="alert">I should render</Text></Loader>)
+
+        let notFound = false;
+        try {
+            screen.getByLabelText("Loading");
+        } catch(e) {
+            notFound = true;
+        }
+
+        expect(notFound).toBeTruthy();
+
+        expect(screen.getByRole("alert")).toHaveTextContent("I should render");
+    });
+
+    test('null, spinner should not render, but content should still render', ()=> {
+        render(<Loader><Text accessibilityRole="alert">I should render</Text></Loader>)
+
+        let notFound = false;
+        try {
+            screen.getByLabelText("Loading");
+        } catch(e) {
+            notFound = true;
+        }
+
+        expect(notFound).toBeTruthy();
+
+        expect(screen.getByRole("alert")).toHaveTextContent("I should render");
+    })
+
+    test('true, content and overlay should render, 1 second later, spinner should render', ()=> {
+        render(<Loader loading={true}><Text accessibilityRole="alert">I should render</Text></Loader>);
+        expect(screen.getByLabelText('Content Cover').props.style.display).toBe(undefined);
+        expect(screen.getByTestId('spinner')).toHaveStyle({display: 'none'});
+        expect(screen.getByRole("alert")).toHaveTextContent("I should render");
+
+        jest.advanceTimersByTime(1000);
+        // // this is not possible to test, because of the issue with the testing library reported here:
+        // // https://github.com/callstack/react-native-testing-library/issues/1069
+        // expect(screen.getByTestId("spinner")).toHaveStyle({display: 'flex'});
+    })
+
+});
